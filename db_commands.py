53056092acb55a6c8aa48b923dc2a07f2a873f53
utsiye <109614537+utsiye@users.noreply.github.com>
2023-11-19 18:57:22 +0100

    Update db_commands.py

--- db_commands.py
+++ db_commands.py
@@ -1,6 +1,9 @@
-from main import pool
+from main import get_pool
 
 class DBCommands:
+    def __init__(self):
+        self.pool = get_pool()
+    
     async def get_all_users(self):
         #with pool.acquire() as conn:
         #    ...
