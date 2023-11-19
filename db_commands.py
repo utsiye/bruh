f2b6a05fda4b5bf7152cdc198dd55b0ee6a118ba
utsiye <109614537+utsiye@users.noreply.github.com>
2023-11-19 18:59:22 +0100

    Update db_commands.py

--- db_commands.py
+++ db_commands.py
@@ -1,8 +1,8 @@
-from main import get_pool
+from app.services.db.db_models import create_db
 
 class DBCommands:
     def __init__(self):
-        self.pool = get_pool()
+        self.pool = await create_db()
     
     async def get_all_users(self):
         #with pool.acquire() as conn:
