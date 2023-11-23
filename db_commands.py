2717df3e11e8978c10f735190e28f13a785a90a8
utsiye <109614537+utsiye@users.noreply.github.com>
2023-11-23 22:58:15 +0100

    Update db_commands.py

--- db_commands.py
+++ db_commands.py
@@ -1,8 +1,12 @@
+from fastapi import Depends
 
+from bla import db_plug
 
 class DBCommands:
-    def __init__(self, pool: Pool):
-        self.pool = pool
+    def __init__(self, pool: Pool = Depends(db_plug)):
+        print(pool) # Depends(db_plug)
+        print(type(pool)) # <class 'fastapi.params.Depends'>
+        self.pool: Pool = pool
     
     async def get_all_users(self):
         #with pool.acquire() as conn:
