a1abe7653bf739faaac0efcfd8a241dcb65cd72d
utsiye <109614537+utsiye@users.noreply.github.com>
2023-11-23 17:54:00 +0100

    Update example_of_the_route.py

--- example_of_the_route.py
+++ example_of_the_route.py
@@ -1,9 +1,13 @@
-from db_commands import DBCommands
+from fastapi import FastAPI, Depends
+
+from app.services.db.db_commands import DBCommands
+from main import appPlug
 
 db = DBCommands()
 
-
-@app.get("/bruh/")
-async def bruh_():
-  users = await db.get_all_users()
+async def register_user_route(...):
   ...
+
+
+def register_route(app: appPlug = Depends()):
+    app.add_route("/register", register_user_route, ['GET'])
