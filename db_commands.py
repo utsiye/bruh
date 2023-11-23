1b1ab13b1638abf1a34a47c15286da3fab4fb264
utsiye <109614537+utsiye@users.noreply.github.com>
2023-11-23 19:59:18 +0100

    Update db_commands.py

--- db_commands.py
+++ db_commands.py
@@ -1,11 +1,8 @@
-from app.services.db.db_models import create_db
-from main import poolPlug
 
-from fastapi import Depends
 
 class DBCommands:
-    def __init__(self, pool: poolPlug = Depends()):
-        self.pool: Pool = pool
+    def __init__(self, pool: Pool):
+        self.pool = pool
     
     async def get_all_users(self):
         #with pool.acquire() as conn:
